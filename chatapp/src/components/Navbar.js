a52719a4f51b2f2e2b352971c0e7892662d0af7d
manoj <manojpawane@gmail.com>
2018-11-27 10:24:22 +0530

    minor changes added

--- chatapp/src/components/Navbar.js
+++ chatapp/src/components/Navbar.js
@@ -33,7 +33,7 @@ class Navbar extends Component {
         )
 
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark- rounded">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
                 <button className="navbar-toggler"
                     type="button"
                     data-toggle="collapse"
--- chatapp/src/components/UserFunctions.js
+++ chatapp/src/components/UserFunctions.js
@@ -1,28 +1,29 @@
 import axios from 'axios'
 
-export const register = newUser =>{
+
+export const register = newUser => {
     return axios
-    .post('user/create',{
-        name:newUser.name,
-        email:newUser.email,
-        password:newUser.password
-    })
-    .then(res => {
-        console.log('Registered.')
-    })
+        .post('user/create', {
+            name: newUser.name,
+            email: newUser.email,
+            password: newUser.password
+        })
+        .then(res => {
+            console.log('Registered.')
+        })
 }
 
-export const login = user =>{
+export const login = user => {
     return axios
-    .post('user/login',{
-        email:user.email,
-        password:user.password
-    })
-    .then(res=>{
-        localStorage.setItem('usertoken', res.data)
-        return res.data
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .post('user/login', {
+            email: user.email,
+            password: user.password
+        })
+        .then(res => {
+            localStorage.setItem('usertoken', res.data)
+            return res.data
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
